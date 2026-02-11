01b1a231d6703a7c3637fccb5533fb5b1aaaa553
codewithshadyy <kipkoechshadrack10@gmail.com>
2026-02-11 10:13:18 +0000

    configuring JWT

--- config/settings.py
+++ config/settings.py
@@ -123,3 +123,20 @@ STATIC_URL = 'static/'
 # authentication model
 AUTH_USER_MODEL = 'users.User'
 
+
+
+# Configuring JWT
+from datetime import timedelta
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=30),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'ROTATE_REFRESH_TOKENS': True,
+    'BLACKLIST_AFTER_ROTATION': True,
+}
