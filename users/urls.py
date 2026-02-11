cf6a4c8e57e65d602723021f4af8557db26ab455
codewithshadyy <kipkoechshadrack10@gmail.com>
2026-02-11 11:08:18 +0000

    implementing user logout feature

--- users/urls.py
+++ users/urls.py
@@ -4,10 +4,11 @@ from rest_framework_simplejwt.views import(
     TokenRefreshView
 )
 
-from .views import UserRegistrationView
+from .views import UserRegistrationView, UserLogOutView
 
 urlpatterns = [
     path("auth/register/", UserRegistrationView.as_view(), name="register"),
     path("auth/login/", TokenObtainPairView.as_view()),
-    path("auth/refresh", TokenRefreshView.as_view()),
+    path("auth/refresh/", TokenRefreshView.as_view()),
+    path("auth/logout/", UserLogOutView.as_view())
 ]
--- users/views.py
+++ users/views.py
@@ -3,8 +3,35 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 from .serializers import RegisterSerializer
 
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from rest_framework import status
+from rest_framework_simplejwt.tokens import RefreshToken
 
+# user registration view
 class UserRegistrationView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = RegisterSerializer
+
+
+class UserLogOutView(APIView):
+    permission_classes = [IsAuthenticated]
+        
+    def post(self, request):
+        try:
+            refresh_token = request.dat["refresh"]
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+            
+            return Response(
+                {"message":"logged out successfully"},
+                status=status.HTTP_205_RESET_CONTENT
+            )
+        except Exception:
+            return Response(
+                {"error":"inavalid token"},
+                status=status.HTTP_400_BAD_REQUEST
+            )    
     
+        
