e64fe9eb4570d02c1c70e64584e71c8a7a01b50b
codewithshadyy <kipkoechshadrack10@gmail.com>
2026-02-12 18:16:20 +0000

    implementing  markets app model

---
+++ markets/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.conf import settings
+
+
+class Market(models.Model):
+    name = models.CharField(max_length=255)
+    location = models.CharField(max_length=255)
+    created_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="markets"
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f"{self.name}-{self.location}"
