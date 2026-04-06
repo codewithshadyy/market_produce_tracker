f9498a22e709a842da43ac7f1ffd5da050f59fe7
codewithshadyy <kipkoechshadrack@gmail.com>
2026-04-06 12:50:45 +0300

    updating documentation format

--- config/urls.py
+++ config/urls.py
@@ -34,6 +34,16 @@ schema_view = get_schema_view(
    permission_classes=(permissions.AllowAny,),
 )
 
+from django.urls import re_path
+
+urlpatterns = [
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$',
+        schema_view.without_ui(cache_timeout=0),
+        name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0)),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0)),
+]
+
 # urlpatterns = [
 #     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0)),
 #     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0)),
@@ -45,6 +55,17 @@ urlpatterns = [
     path("api/", include("markets.urls")),
     path("api/", include("produces.urls")),
     path('api/', include("alerts.urls")),
+    # path('swagger/', schema_view.with_ui('swagger', cache_timeout=0)),
+    # path('redoc/', schema_view.with_ui('redoc', cache_timeout=0)),
+]
+
+
+urlpatterns += {
+
+       re_path(r'^swagger(?P<format>\.json|\.yaml)$',
+        schema_view.without_ui(cache_timeout=0),
+        name='schema-json'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0)),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0)),
-]
+
+}
